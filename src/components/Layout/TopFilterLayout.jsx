145f56d7876092c150b8eae8f928affdd1a93993
kreafox <krisztina.n.elekes@gmail.com>
2022-06-15 11:41:47 +0300

    Remove empty columns from search footer

--- src/components/Layout/TopFilterLayout.jsx
+++ src/components/Layout/TopFilterLayout.jsx
@@ -31,25 +31,9 @@ const TopFilterLayout = (props) => {
 
       <Grid className="body-footer">
         <Grid.Row>
-          <Grid.Column
-            widescreen="2"
-            tablet="2"
-            computer="2"
-            className="col-left"
-          ></Grid.Column>
-          <Grid.Column
-            widescreen="8"
-            tablet="12"
-            computer="10"
-            className="col-mid"
-          >
+          <Grid.Column>
             <div>{bodyFooter}</div>
           </Grid.Column>
-          <Grid.Column
-            only="computer widescreen large screen"
-            widescreen="2"
-            className="col-right"
-          ></Grid.Column>
         </Grid.Row>
       </Grid>
     </div>
